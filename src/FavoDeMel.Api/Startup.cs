c435fdce0287c666c82a650a073db657017f9f4e
Igor Jordany <igor.silva@nuvem.net>
2020-07-03 14:33:14 -0400

    Adicionado Swagger e configuração docker

--- src/FavoDeMel.Api/Startup.cs
+++ src/FavoDeMel.Api/Startup.cs
@@ -29,16 +29,24 @@ namespace FavoDeMel.Api
             services.AddControllers();
 
             services.ResolveDependencies();
+
+            services.AddSwaggerGen();
         }
         
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            UpdateDatabase(app);
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -49,6 +57,18 @@ namespace FavoDeMel.Api
             {
                 endpoints.MapControllers();
             });
+        }
+        private static void UpdateDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<DatabaseContext>())
+                {
+                    context.Database.Migrate();
+                }
+            }
         }
     }
 }
